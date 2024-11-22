15f55b0fea9ddea9c8df45a6a3a36c33ec4b3709
Thomas Medina <90817405+viegosabroso@users.noreply.github.com>
2024-11-21 19:27:32 -0500

    Se intento ://///

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Cards from './components/Cards/Cards'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
